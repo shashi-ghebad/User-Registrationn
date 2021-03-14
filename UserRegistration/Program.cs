f6cd01ddd9c977411377dee36ac94d4193b1d7d1
Shashi <shashihghebad3@gmail.com>
2021-03-13 22:13:43 -0800

    [SHASHI]Add:UC1-ValidName

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        public static string userName;
+
+        public static bool UserName(string userName)
+        {
+            return Regex.IsMatch(userName, @"^[A-Z]{1}[a-zA-Z0-9]{2,}$");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Valid User Name: ");
+            userName = Console.ReadLine();
+            Console.WriteLine(UserName(userName));
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
