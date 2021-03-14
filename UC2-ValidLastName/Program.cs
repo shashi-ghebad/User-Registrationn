c0dac34723ec0b267bcbaa8b5243761d104c119c
Shashi <shashihghebad3@gmail.com>
2021-03-13 22:41:35 -0800

    [SHASHI]Add:UC2-ValidLastName

--- UC2-ValidLastName/Program.cs
+++ UC2-ValidLastName/Program.cs
@@ -10,25 +10,29 @@ namespace UserRegistration
     class Program
     {
         public static string userName;
+        public static string lastName;
 
         public static bool UserName(string userName)
         {
-            return Regex.IsMatch(userName, @"^[A-Z]{1}[a-zA-Z0-9]{2,}$");
+            return Regex.IsMatch(userName, @"^[A-Z]{1}[a-z0-9A-Z]{2,}$");
+        }
+
+        public static bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, @"^[A-Z]{1}[a-z0-9A-Z]{2,}$");
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a Valid User Name: ");
             userName = Console.ReadLine();
-            Console.WriteLine(UserName(userName));
+            Console.WriteLine(UserName(userName) + "\n");
+
+            Console.WriteLine("Enter a Valid Last Name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine(UserName(lastName) + "\n");
+
             Console.ReadKey();
         }
     }
 }
-
-
-
-
-
-
-
